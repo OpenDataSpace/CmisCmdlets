aa0d5249fc48af68133fa34a6e74b866102ac1ad
Stefan Burnicki <stefan.burnicki@burnicki.net>
2014-10-27 17:07:51 +0100

    Cmdlet for querying properties

--- Source/CmisCmdlets/GetCmisPropertyCommand.cs
+++ Source/CmisCmdlets/GetCmisPropertyCommand.cs
@@ -8,12 +8,68 @@
 // not distributed with this file, You can obtain one at
 //  http://mozilla.org/MPL/2.0/.
 using System;
+using System.Linq;
+using System.Management.Automation;
+using DotCMIS.Client;
+using DotCMIS;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace CmisCmdlets
 {
-    public class GetCmisPropertyCommand
+    [Cmdlet(VerbsCommon.Get, "CmisProperty")]
+    public class GetCmisPropertyCommand : CmisCommandBase
     {
+        [Parameter(Mandatory = false, Position = 1, ValueFromPipeline = true)]
+        public ICmisObject Object { get; set; }
 
+        [Parameter(Mandatory = false, Position = 0)]
+        public string Property { get; set; }
+
+
+        private static Dictionary<string, string> _existingProperties;
+
+        static GetCmisPropertyCommand()
+        {
+            var flags = BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.Public;
+            var fields = from prop in typeof(PropertyIds).GetFields(flags) select prop;
+            _existingProperties = new Dictionary<string, string>(
+                StringComparer.InvariantCultureIgnoreCase);
+            foreach (var curField in fields)
+            {
+                var value = curField.GetValue(null);
+                if (value is string)
+                {
+                    _existingProperties.Add(curField.Name, (string) value);
+                }
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (String.IsNullOrEmpty(Property))
+            {
+                if (Object == null)
+                {
+                    WriteObject(_existingProperties);
+                }
+                else
+                {
+                    WriteObject(Object.Properties);
+                }
+                return;
+            }
+
+            var wildcard = new WildcardPattern(Property + "*", WildcardOptions.IgnoreCase);
+            if (Object == null)
+            {
+                WriteObject(from pair in _existingProperties where wildcard.IsMatch(pair.Key)
+                            select pair.Value, true);
+                return;
+            }
+            WriteObject(from prop in Object.Properties where wildcard.IsMatch(prop.LocalName)
+                        select prop, true);
+        }
     }
 }
 
