cfcf1d886af89e997c98d506d91801e4e5947dd8
Stefan Burnicki <stefan.burnicki@burnicki.net>
2015-08-18 13:40:09 +0200

    New Provider.Test project to test the provider

---
+++ Source/Provider.Test/NewDriveTests.cs
@@ -0,0 +1,33 @@
+﻿// CmisCmdlets - Cmdlets to use CMIS from Powershell and Pash
+// Copyright (C) GRAU DATA 2013-2014
+//
+// Author(s): Stefan Burnicki <stefan.burnicki@graudata.com>
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
+//  http://mozilla.org/MPL/2.0/.
+using System;
+using System.Linq;
+using NUnit.Framework;
+using CmisProvider;
+
+namespace CmisProvider.Test
+{
+    [TestFixture]
+    public class NewDriveTests : ProviderTestBase
+    {
+        [Test]
+        public void CreateNewDrive()
+        {
+            var cmd = GetNewDriveCommand();
+            var res = Shell.Execute(cmd);
+            Assert.That(res.Count, Is.EqualTo(1));
+            var drive = res.First() as CmisDrive;
+            Assert.That(drive, Is.Not.Null);
+            Assert.That(drive.Connection, Is.Not.Null);
+            Assert.That(drive.Connection.RepositoryInfo.Name, Is.EqualTo(TestRepository));
+        }
+    }
+}
+
---
+++ Source/Provider.Test/ProviderTestBase.cs
@@ -0,0 +1,36 @@
+﻿// CmisCmdlets - Cmdlets to use CMIS from Powershell and Pash
+// Copyright (C) GRAU DATA 2013-2014
+//
+// Author(s): Stefan Burnicki <stefan.burnicki@graudata.com>
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL was
+// not distributed with this file, You can obtain one at
+//  http://mozilla.org/MPL/2.0/.
+using System;
+using CmisUtility.Test;
+using CmisProvider;
+
+namespace CmisProvider.Test
+{
+    public class ProviderTestBase : CmisTestBase
+    {
+        public ProviderTestBase() : base(typeof(CmisProvider))
+        {
+        }
+
+        protected string GetNewDriveCommand()
+        {
+            var credential = CreateCredentialCommand(TestUser, TestPassword);
+            return "New-PSDrive -PSProvider 'Cmis' -Name 'CmisTest' -Credential " + credential +
+                   " -Host '" + TestURL + "' -Root '" + TestRepository + ":/'";
+        }
+
+        protected string CreateCredentialCommand(string username, string password)
+        {
+            return "(New-Object System.Management.Automation.PSCredential ('" + username + "', (" +
+                    "ConvertTo-SecureString '" + password + "' -AsPlainText -Force )))";
+        }
+    }
+}
+
